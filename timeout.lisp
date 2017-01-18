26beb3362e50547b2beab451af9c8991574e9250
peyton <peyton@peytonfarrar.com>
2017-01-17 21:02:18 -0900

    Improved timeout error messages

--- timeout.lisp
+++ timeout.lisp
@@ -2,14 +2,11 @@
 
 (in-package #:timeout)
 
-(define-condition timeout-error (error)
-  ((text :initarg text :reader text)))
-
 (defmacro with-timeout (timeout &body body)
   `(let ((thread (bt:make-thread #'(lambda () ,@body))))
      (sleep ,timeout)
      (if (bt:thread-alive-p thread)
          (progn (bt:destroy-thread thread)
-                (error 'timeout-error :text
-                       "A timeout was issued on ~A after ~A seconds."))
+                (error (format nil "A timeout was issued on ~A after ~A second(s)."
+                               thread ,timeout)))
          (bt:join-thread thread))))
